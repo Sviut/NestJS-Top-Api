1e61378ebadffadf3c9d9467d57d1fb8364e9cf4
Sviut <jeleznov88@gmail.com>
2021-06-24 17:21:45 +0300

    add login logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common'
 import { TypegooseModule } from 'nestjs-typegoose'
 import { AuthController } from './auth.controller'
 import { UserModel } from './user.model'
-import { AuthService } from './auth.service';
+import { AuthService } from './auth.service'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { getJWTConfig } from '../configs/jwt.config'
 
 @Module({
   controllers: [AuthController],
@@ -15,6 +18,11 @@ import { AuthService } from './auth.service';
         },
       },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig,
+    }),
   ],
   providers: [AuthService],
 })
